efa7a3a0974da4812f0f3d5d81780d5014eae8f7
Zachariah Ngonyani <zachariahn@gmail.com>
2020-04-06 23:55:51 +0300

    Add functions to determine a hit or miss

---
+++ lib/islands_engine/coordinate.ex
@@ -0,0 +1,28 @@
+defmodule IslandsEngine.Coordinate do
+  defstruct in_island: :none, guessed?: false
+
+  alias IslandsEngine.Coordinate
+
+  def start_link do
+    Agent.start_link(fn -> %Coordinate{} end)
+  end
+
+  def guessed?(coordinate) do
+    Agent.get(coordinate, fn state -> state.guessed? end)
+  end
+
+  def island(coordinate) do
+    Agent.get(coordinate, fn state -> state.in_island end)
+  end
+
+  def in_island?(coordinate) do
+    case island(coordinate) do
+      :none -> false
+      _any  -> true
+    end
+  end
+
+  def hit?(coordinate) do
+    in_island?(coordinate) && guessed?(coordinate)
+  end
+end
